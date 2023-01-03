6deea9ddd1edd0daad61fec1e4047fdcd4cacb81
Tiago Santos <tfsantosbr@gmail.com>
2023-01-02 21:04:28 -0300

    configure typescript

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,17 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
 import { companyMainActivityRoutes } from './routes/company-main-activities.routes';
 import { partnerRoutes } from './routes/partners.routes';
 
+dotenv.config();
+
 const app = express();
+const serverPort = process.env.SERVER_PORT;
 
 app.use(express.json());
 
 app.use('/partners', partnerRoutes);
+app.use('/company-main-activities', companyMainActivityRoutes);
 
-app.listen(3333, () => console.log("Server is running on port: 3333"));
+app.listen(serverPort, () => console.log(`Server is running on port: ${serverPort}`));
